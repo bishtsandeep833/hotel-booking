b4fa25a5ce0da49d7311e02c937774bb22d1df69
Sandeep Bisht <bishtsandeep833@gmail.com>
2026-05-14 20:48:13 +0530

    add dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home'
 import Footer from './components/Footer'
 import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
+import MyBookings from './pages/MyBookings'
+import HotelReg from './components/HotelReg'
 
 const App = () => {
 
@@ -13,11 +15,13 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      <HotelReg />
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/rooms' element={<AllRooms/>} />
           <Route path='/rooms/:id' element={<RoomDetails/>} />
+          <Route path='/my-bookings' element={<MyBookings/>} />
         </Routes>
       </div>
       <Footer />
